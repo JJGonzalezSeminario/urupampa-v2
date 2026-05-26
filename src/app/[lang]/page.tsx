71ad03898857d3049675ce0e436c20bc245901d6
Jorge <chilenojorge@yahoo.de>
2026-05-26 21:19:36 +0200

    Fix: Prop lang agregada a Shows

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -25,7 +25,7 @@ export default async function Home({ params }: { params: Promise<{ lang: string 
       <About dict={dict.about} />
       <Media dict={dict.media} />
       <SocialProof dict={dict.social_proof} />
-      <Shows dict={dict.shows} />
+      <Shows dict={dict.shows} lang={lang} />
       <Gallery dict={dict.gallery} />
       <Booking dict={dict.booking} />
     </main>
